c97f52047415ff22daf7eff60a52ae36f169bca9
Matej Vykoukal <61636415+MatejVykoukal@users.noreply.github.com>
2022-09-17 17:04:02 +0200

    Add cms

---
+++ pages/_document.js
@@ -0,0 +1,28 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+	return (
+		<Html>
+			<Head />
+			<body>
+				<Main />
+				<NextScript />
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `
+    if (window.netlifyIdentity) {
+      window.netlifyIdentity.on("init", user => {
+        if (!user) {
+          window.netlifyIdentity.on("login", () => {
+            document.location.href = "/admin/";
+          });
+        }
+      });
+    }
+  `,
+					}}
+				></script>
+			</body>
+		</Html>
+	);
+}
